b5a4fbc3f459afefab1e5aa065eabdbee47574d1
Кирилл Ботатин (ноут) <botanin.k@mail.ru>
2024-11-27 20:37:45 +0500

    style: Improve code readability
    - Made minor adjustments in main.js, including formatting and comments, to enhance code readability.

--- main.js
+++ main.js
@@ -3,14 +3,14 @@ import * as THREE from 'https://cdn.jsdelivr.net/npm/three/build/three.module.js
 // Сцена
 const scene = new THREE.Scene();
 
-// Камера
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 5;
-
-// renderer
+// Отрисовщик
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+// Камера
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
 // Куб
 const geometry = new THREE.BoxGeometry(1, 1, 1);
@@ -19,10 +19,10 @@ const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
 // Рёбра
-const edges = new THREE.EdgesGeometry(geometry); // Создаем ребра из геометрии куба
-const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff }); // Черный цвет для ребер
+const edges = new THREE.EdgesGeometry(geometry);
+const lineMaterial = new THREE.LineBasicMaterial({ color: 0xffffff });
 const wireframe = new THREE.LineSegments(edges, lineMaterial);
-scene.add(wireframe); // Добавляем ребра в сцену
+scene.add(wireframe);
 
 // Анимация
 function animate() {
@@ -31,7 +31,7 @@ function animate() {
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
 
-  wireframe.rotation.x = cube.rotation.x; // Синхронизируем вращение
+  wireframe.rotation.x = cube.rotation.x;
   wireframe.rotation.y = cube.rotation.y;
 
   renderer.render(scene, camera);
